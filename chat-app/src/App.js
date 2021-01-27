2803a1fe320951b390a7feb37f25c3a8e0497314
BelaM80 <mueller.bela@gmx.de>
2021-01-27 12:24:57 +0100

    Insall CRA and firebase

---
+++ chat-app/src/App.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+function App() {
+  return (
+    <div>
+    
+    </div>
+  );
+}
+
+export default App;
